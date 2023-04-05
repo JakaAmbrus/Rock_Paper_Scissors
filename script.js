30b8e317b030a25007f0dc208add40121de2d2e6
JakaAmbrus <email=126106951+JakaAmbrus@users.noreply.github.com>
2023-04-06 00:21:16 +0200

    the first step in making the game, very early stage

---
+++ script.js
@@ -0,0 +1,44 @@
+function getComputerChoice(){
+let random_number = Math.floor(Math.random() * 3);
+let  ComputerSelection;
+    if(random_number == 0){
+    return ComputerSelection = "rock";
+}
+    else if(random_number == 1){
+    return ComputerSelection = "paper";
+}
+    else if(random_number == 2){
+    return ComputerSelection = "scissors";
+}
+
+}
+function playRound(x, y){
+    if (x == "rock" && y == "paper"){
+        return "ah you lose";
+    }
+    else if (x == "rock" && y == "scissors"){
+        return "you won";
+    }
+    else if (x == "paper" && y == "scissors"){
+        return "ah you lose";
+    }
+    else if (x == "paper" && y == "rock"){
+        return "you won";
+    }
+    else if (x == "scissors" && y == "rock"){
+        return "ah you lose";;
+    }
+    else if (x == "scissors" && y == "paper"){
+        return "you won";
+    }
+    else if (x == y){
+        return "you tied"
+    }
+
+}
+let computer = getComputerChoice();
+let player = "rock";
+let result = playRound(player, computer);
+console.log("you chose " + player);
+console.log("computer chose " + computer);
+console.log(result);
