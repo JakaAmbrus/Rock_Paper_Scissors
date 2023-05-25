783790952d8d77781344a67330db09df67b0ef4d
Jaka <email=126106951+JakaAmbrus@users.noreply.github.com>
2023-05-25 18:23:29 +0300

    made the style of my website game

--- script.js
+++ script.js
@@ -2,25 +2,42 @@
 const startBtn = document.querySelector('.start-btn');
 const overlay = document.querySelector('#overlay');
 const gameContainer = document.querySelector('#game-container');
-
-startBtn.addEventListener('click', function() {
+const textBelowTitle = document.querySelector('#text-below-title');
+const title = document.querySelector('#title');
+const titleR = document.querySelector('.title-r');
+const titleP = document.querySelector('.title-p');
+const titleS = document.querySelector('.title-s');
+const choices = document.querySelector('#choices-container');
+const scoresContainer = document.querySelector('#scores');
+console.log('3')
+//transition to the game from start game screen
+startBtn.addEventListener('click', hideOverlayAndGameStart);
+function hideOverlayAndGameStart()
+{
     gameContainer.style.opacity = '1';
     overlay.style.opacity = '0';
     setTimeout(() => {
         overlay.style.display = "none";
         gameContainer.style.display = 'grid';
-        // gameContainer.style.width = '100%';
-        // gameContainer.style.height = '100vh';
-      }, 400);
-    
-  });
-
-
-function hideOverlay()
-{
-    overlay.style.display = 'none';
+        setTimeout(() => {
+            titleR.style.opacity = '1';
+            setTimeout(() => {
+                titleP.style.opacity = '1';
+                setTimeout(() => {
+                    titleS.style.opacity = '1';
+                    setTimeout(() => {
+                        textBelowTitle.style.opacity = '1';
+                        setTimeout(() => {
+                            scoresContainer.style.opacity = '1';
+                            choices.style.opacity = '1';
+                            
+                        }, 1200)    
+                    }, 1000)
+                }, 700)
+            }, 700);
+          }, 700);
+        }, 600);
 }
-
 
 // function getComputerChoice(){
 // let random_number = Math.floor(Math.random() * 3);
@@ -66,3 +83,26 @@ function hideOverlay()
 // console.log("you chose " + player);
 // console.log("computer chose " + computer);
 // console.log(result);
+
+/*
+I WAS TRYING TO MAKE THE ANIMATION OF THE TITLE SCREEN WAY TOO COMPLICATED
+function displayWords(i) {
+    if (i < titleArr.length) {
+      const wordElement = document.createElement("span");
+      wordElement.textContent = titleArr[i];
+
+      // Add a space after each word except for the last one
+      if (i < titleArr.length - 1) {
+        const spaceElement = document.createTextNode(" ");
+        title.appendChild(spaceElement);
+      }
+
+      title.appendChild(wordElement);
+      void wordElement.offsetWidth;
+      wordElement.style.opacity = "1";
+      setTimeout(() => {
+        displayWords(i + 1);
+      }, 1000);
+    }
+  }
+*/
