8e69590e6bd2c84ecf14a6157b6d6daabfce399f
Jaka <email=126106951+JakaAmbrus@users.noreply.github.com>
2023-05-25 23:51:52 +0300

    now all that is left is the animations

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const titleP = document.querySelector('.title-p');
 const titleS = document.querySelector('.title-s');
 const choices = document.querySelector('#choices-container');
 const scoresContainer = document.querySelector('#scores');
+
 console.log('3fddfhg')
 //transition to the game from start game screen
 startBtn.addEventListener('click', hideOverlayAndGameStart);
@@ -22,7 +23,7 @@ function hideOverlayAndGameStart()
         setTimeout(() => {
             titleR.style.opacity = '1';
             setTimeout(() => {
-                titleP.style.opacity = '1';
+                titleP.style.opacity = '1'; //I know I am nesting a lot, will improve on this in the next projects
                 setTimeout(() => {
                     titleS.style.opacity = '1';
                     setTimeout(() => {
@@ -39,51 +40,121 @@ function hideOverlayAndGameStart()
         }, 600);
 }
 
-// function getComputerChoice(){
-// let random_number = Math.floor(Math.random() * 3);
-// let  ComputerSelection;
-//     if(random_number == 0){
-//     return ComputerSelection = "rock";
-// }
-//     else if(random_number == 1){
-//     return ComputerSelection = "paper";
-// }
-//     else if(random_number == 2){
-//     return ComputerSelection = "scissors";
-// }
+let playerScore = 0;
+let computerScore = 0;
+const rButton = document.querySelector('#rock');
+const pButton = document.querySelector('#paper');
+const sButton = document.querySelector('#scissors');
+let playerScoreSpan = document.querySelector('#player-score-span');
+let computerScoreSpan = document.querySelector('#computer-score-span');
 
-// }
-// function playRound(x, y){
-//     if (x == "rock" && y == "paper"){
-//         return "ah you lose";
-//     }
-//     else if (x == "rock" && y == "scissors"){
-//         return "you won";
-//     }
-//     else if (x == "paper" && y == "scissors"){
-//         return "ah you lose";
-//     }
-//     else if (x == "paper" && y == "rock"){
-//         return "you won";
-//     }
-//     else if (x == "scissors" && y == "rock"){
-//         return "ah you lose";;
-//     }
-//     else if (x == "scissors" && y == "paper"){
-//         return "you won";
-//     }
-//     else if (x == y){
-//         return "you tied"
-//     }
+function playerSelection(){
+    rButton.addEventListener('click', () => {
+        playRound('rock', getComputerChoice())
+    });
+    pButton.addEventListener('click', () => {
+        playRound('paper', getComputerChoice())
+    });
+    sButton.addEventListener('click', () => {
+        playRound('scissors', getComputerChoice())
+    });
+}
 
-// }
-// let computer = getComputerChoice();
-// let player = "rock";
-// let result = playRound(player, computer);
-// console.log("you chose " + player);
-// console.log("computer chose " + computer);
-// console.log(result);
+function getComputerChoice(){
+let random_number = Math.floor(Math.random() * 3);
+let  ComputerSelection;
+    if(random_number == 0){
+    return ComputerSelection = "rock";
+}
+    else if(random_number == 1){
+    return ComputerSelection = "paper";
+}
+    else if(random_number == 2){
+    return ComputerSelection = "scissors";
+}
 
+}
+function playRound(x, y){
+    if (x == "rock" && y == "paper"){
+        return winRP();
+    }
+    else if (x == "rock" && y == "scissors"){
+        return loseRS();
+    }
+    else if (x == "paper" && y == "scissors"){
+        return losePS();
+    }
+    else if (x == "paper" && y == "rock"){
+        return winPR();
+    }
+    else if (x == "scissors" && y == "rock"){
+        return loseSR();;
+    }
+    else if (x == "scissors" && y == "paper"){
+        return winSP();
+    }
+    else if (x == "rock" && y == "rock"){
+        return tieR();
+    }
+    else if (x == "paper" && y == "paper"){
+        return tieP();
+    }
+    else if (x == "scissors" && y == "scissors"){
+        return tieS();
+    }
+
+}
+function win(){
+    playerScore++;
+    playerScoreSpan.innerHTML = playerScore;
+    computerScoreSpan.innerHTML = computerScore;
+    console.log('win')
+    if(playerScore == 5){
+        gameReset();
+        
+    }
+
+}
+function lose(){
+    computerScore++;
+    playerScoreSpan.innerHTML = playerScore;
+    computerScoreSpan.innerHTML = computerScore;
+    console.log('lose')
+    if(computerScore == 5){
+        gameReset();
+    }
+}
+function tie(){
+    console.log('tie')
+}
+function gameReset(){
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreSpan.innerHTML = playerScore;
+    computerScoreSpan.innerHTML = computerScore;
+}
+function winRP(){
+    win();
+}
+function winPR(){
+    win();
+}
+function winSP(){
+    win();
+}
+
+function losePS(){
+    lose();
+}
+function loseRS(){
+    lose();
+}
+function loseSR(){
+    lose();
+}
+
+
+playerSelection();
 /*
 I WAS TRYING TO MAKE THE ANIMATION OF THE TITLE SCREEN WAY TOO COMPLICATED
 function displayWords(i) {
