6076e35cbf6a73be24bf4122c2f729cc04553be9
Jaka <email=126106951+JakaAmbrus@users.noreply.github.com>
2023-05-25 18:30:11 +0300

    dfs

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const titleP = document.querySelector('.title-p');
 const titleS = document.querySelector('.title-s');
 const choices = document.querySelector('#choices-container');
 const scoresContainer = document.querySelector('#scores');
-console.log('3')sdgdsd
+console.log('3fddfhg')
 //transition to the game from start game screen
 startBtn.addEventListener('click', hideOverlayAndGameStart);
 function hideOverlayAndGameStart()
